fbc793b89817d13da8ac8186ba3253f86d63765d
Tonni <hby3387@gmail.com>
2025-05-08 21:47:26 +0930

    Fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Calendar from '@/components/Calendar/Calendar';
+import { Suspense } from 'react';
 import HolidaysInYear from '@/components/HolidaysInYear/HolidaysInYear';
 import RegionSelect from '@/components/RegionSelect/RegionSelect';
 import Col from 'react-bootstrap/Col';
@@ -9,24 +10,26 @@ import Row from 'react-bootstrap/Row';
 
 export default function Home() {
   return (
-    <Container>
-      <Row>
-        <Col className="col-lg-3 col-12">
-          <RegionSelect />
-          <HolidaysInYear />
-        </Col>
-        <Col className="col-lg-9 col-12">
-          <Calendar />
-          <div className="text-end my-3">
-            <a
-              className="text-body-tertiary fst-italic fs-6"
-              href="https://forms.gle/QSAnrKbDnTtQcZbB7"
-            >
-              🙋 Report incorrect holiday
-            </a>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <Suspense>
+      <Container>
+        <Row>
+          <Col className="col-lg-3 col-12">
+            <RegionSelect />
+            <HolidaysInYear />
+          </Col>
+          <Col className="col-lg-9 col-12">
+            <Calendar />
+            <div className="text-end my-3">
+              <a
+                className="text-body-tertiary fst-italic fs-6"
+                href="https://forms.gle/QSAnrKbDnTtQcZbB7"
+              >
+                🙋 Report incorrect holiday
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </Suspense>
   );
 }
